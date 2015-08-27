85b1a7a9dcf0fbeaa080427ed59b2a2c1ec12a4c
Nate Bird <natebird@gmail.com>
2015-08-27 11:41:34 -0700

    Add initial photogallery

---
+++ app/src/main/java/co/natebird/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,11 @@
+package co.natebird.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    public Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+}
